5999e3a81fc1eece8caea1441db4bb60b07b8fdd
arunsoni Soni <arunsoni@nus.edu.sg>
2026-02-25 11:03:39 +0000

    Initial Project Structure

---
+++ coireport/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["nus/edu/sg/coireport/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ coireport/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"nus/edu/sg/coireport/test/unit/controller/CoiHome.controller"
+], function () {
+	"use strict";
+});
